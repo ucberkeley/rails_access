02ab846bc056411ae5bd1f4ba6dcb7b61a0cdee2
SixArm <sixarm@sixarm.com>
2012-08-01 21:37:04 -0700

    Add shim to test helper because controllers need their routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,3 +18,18 @@ end
 if ActionDispatch::IntegrationTest.method_defined?(:fixture_path=)
   ActionDispatch::IntegrationTest.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
+
+# Patch TestCase to provide routes: 
+# See https://github.com/rails/rails/issues/6573
+class ActionController::TestCase
+  def method_missing(selector, *args)
+    if @controller.respond_to?(:_routes) &&
+        ( @controller._routes.named_routes.helpers.include?(selector) ||
+          @controller._routes.mounted_helpers.method_defined?(selector) )
+      @controller.__send__(selector, *args)
+    else
+      super
+    end
+  end    
+end
+
